c1a6f047c2ff653d3715d5beb4fc6ad547c3a344
quangPTM <quangmtp@gmail.com>
2023-10-27 10:14:13 +0700

    code first

---
+++ ManageCategoriesApp/ManageCategoriesApp/MyStockDBContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+public class Category
+{
+    public Category() { }
+    [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int CategoryID { get; set; }
+    public string CategoryName { get; set; }
+}
+public class MyStock : DbContext
+{
+    public MyStock() { }
+    public DbSet<Category> Categories { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        IConfigurationRoot configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("MyStoreDB2"));
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .Property(category => category.CategoryName)
+            .IsRequired()
+            .HasMaxLength(40);
+        modelBuilder.Entity<Category>().HasData(
+            new Category { CategoryID = 1, CategoryName = "Beverages" },
+            new Category { CategoryID = 2, CategoryName = "Condiments" },
+            new Category { CategoryID = 3, CategoryName = "Confections" }
+            );
+    }
+}
---
+++ ManageCategoriesApp/ManageCategoriesApp/frmManageCategories.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ManageCategoriesApp
+{
+    partial class frmManageCategories
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtCategoryID = new TextBox();
+            txtCategoryName = new TextBox();
+            dgvCategories = new DataGridView();
+            btnInsert = new Button();
+            btnUpdate = new Button();
+            txtDelete = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvCategories).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(199, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 20);
+            label1.TabIndex = 0;
+            label1.Text = "CategoryID";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(199, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 20);
+            label2.TabIndex = 1;
+            label2.Text = "CategoryName";
+            // 
+            // txtCategoryID
+            // 
+            txtCategoryID.Location = new Point(348, 38);
+            txtCategoryID.Name = "txtCategoryID";
+            txtCategoryID.Size = new Size(229, 27);
+            txtCategoryID.TabIndex = 2;
+            // 
+            // txtCategoryName
+            // 
+            txtCategoryName.Location = new Point(348, 94);
+            txtCategoryName.Name = "txtCategoryName";
+            txtCategoryName.Size = new Size(229, 27);
+            txtCategoryName.TabIndex = 3;
+            // 
+            // dgvCategories
+            // 
+            dgvCategories.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvCategories.Location = new Point(100, 149);
+            dgvCategories.Name = "dgvCategories";
+            dgvCategories.RowHeadersWidth = 51;
+            dgvCategories.RowTemplate.Height = 29;
+            dgvCategories.Size = new Size(575, 188);
+            dgvCategories.TabIndex = 4;
+            dgvCategories.CellContentClick += dgvCategories_CellContentClick;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(152, 362);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(94, 29);
+            btnInsert.TabIndex = 5;
+            btnInsert.Text = "Insert";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(324, 362);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(94, 29);
+            btnUpdate.TabIndex = 6;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // txtDelete
+            // 
+            txtDelete.Location = new Point(524, 362);
+            txtDelete.Name = "txtDelete";
+            txtDelete.Size = new Size(94, 29);
+            txtDelete.TabIndex = 7;
+            txtDelete.Text = "Delete";
+            txtDelete.UseVisualStyleBackColor = true;
+            txtDelete.Click += txtDelete_Click;
+            // 
+            // frmManageCategories
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnInsert);
+            Controls.Add(dgvCategories);
+            Controls.Add(txtCategoryName);
+            Controls.Add(txtCategoryID);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "frmManageCategories";
+            RightToLeft = RightToLeft.No;
+            Text = "Manage Categories";
+            Load += frmManageCategories_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvCategories).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtCategoryID;
+        private TextBox txtCategoryName;
+        private DataGridView dgvCategories;
+        private Button btnInsert;
+        private Button btnUpdate;
+        private Button txtDelete;
+    }
+}
